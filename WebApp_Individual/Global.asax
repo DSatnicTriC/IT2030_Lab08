85b3ad8a4458d9b8a823168278d7d3e3980892db
IT2030DSatnic <S01031109@acad.tri-c.edu>
2018-04-08 16:35:26 -0400

    Initial commit for project with inidividual autorize attribute + set authorize on controller level

---
+++ WebApp_Individual/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp_Individual.MvcApplication" Language="C#" %>
---
+++ WebApp_Individual/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApp_Individual.Startup))]
+namespace WebApp_Individual
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
